42b5e134f25ff878913df2bff9ac6663a82dd613
Lokhi <vlrpinus@gmail.com>
2020-02-06 13:13:15 -0700

    updating with terminal methods

--- src/com/lokhi/steams/example/JavaSteamsExample.java
+++ src/com/lokhi/steams/example/JavaSteamsExample.java
@@ -2,7 +2,6 @@ package com.lokhi.steams.example;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
@@ -27,7 +26,7 @@ public class JavaSteamsExample {
 		 */
 		List<Integer> numberList= Arrays.asList(1,2,3,4,5,6,7);
 		List<Integer> squareRooteofNumber= numberList.stream().map(x->x*x).collect(Collectors.toList()); 
-		System.out.println(squareRooteofNumber); // will print the square root of each number in numberList
+		System.out.println("syso->"+squareRooteofNumber); // will print the square root of each number in numberList
 		
 		// 2.filter
 		/**
@@ -36,7 +35,7 @@ public class JavaSteamsExample {
 		
 		List<String> stringList = Arrays.asList("Rama","Lokesh","Sitha", "Ntr", "Jeevan","Ravan", "Rajesh");
 		List<String> filterList= stringList.stream().filter(s->s.startsWith("R")).collect(Collectors.toList()); // filtering above list whose name starts with R
-		System.out.println(filterList);
+		System.out.println("syso->"+filterList);
 		
 		//3.sorted
 		
@@ -46,6 +45,28 @@ public class JavaSteamsExample {
 		
 		List<String> namesList = Arrays.asList("Reflection","Collection","Stream", "Java", "8");
 		List<String> sortedList= namesList.stream().sorted().collect(Collectors.toList());
-		System.out.println(sortedList);
+		System.out.println("syso->"+sortedList);
+		
+		// And there are 3 terminal operations.. 
+		//1. Collect
+		/**
+		 * The collect method is used to return the result of the intermediate operations performed on the stream.
+		 * all above examples are using collect
+		 */
+		//2. Foreach
+		/**
+		 * The forEach method is used to iterate through every element of the stream.
+		 */
+		//Example
+		filterList.forEach(s->System.out.println(s));
+		
+		//3. reduce. 
+		/**
+		 * The reduce method is used to reduce the elements of a stream to a single value.
+		   The reduce method takes a BinaryOperator as a parameter.
+		 */
+
+		int evenNum = numberList.stream().filter(x->x%2==0).reduce(0,(ans,i)-> ans+i);
+		System.out.println("syso->"+evenNum);
 	}
 }
